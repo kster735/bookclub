98ca5db1ce4695e19b1e281ba02fc60060a3afc2
Stergios Katsogiannis <kster735@gmial.com>
2024-09-19 16:01:49 +0300

    env var or ''

--- models/config.js
+++ models/config.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 import 'dotenv/config'
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
     dialect: 'postgres',
     // dialectOptions: {
     //     ssl: {
