9a8ab722ddcafe7349c62a28c87b9bac97034cb2
Stergios Katsogiannis <kster735@gmial.com>
2024-09-19 17:06:58 +0300

    removed ssl connections

--- models/config.js
+++ models/config.js
@@ -3,12 +3,12 @@ import 'dotenv/config'
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false
+    //     }
+    // },
     logging: false,
     define: {
         timestamps: false
