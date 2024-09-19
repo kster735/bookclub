1d450ee9becbcfb66cfb18abbf3567126dfb0f09
Stergios Katsogiannis <kster735@gmial.com>
2024-09-19 20:38:01 +0300

    fixing static pubclic url

--- public/js/links.js
+++ public/js/links.js
@@ -1,18 +1,17 @@
-const links=[
-    document.getElementById('books'),
-    document.getElementById('addbookform'),
-    document.getElementById('about')
-]
+const links = [
+  document.getElementById("/bookclub/books"),
+  document.getElementById("/bookclub/addbookform"),
+  document.getElementById("/bookclub/about"),
+];
 
-links.forEach( (elem) =>{
-    const url = location.href.toString().split('/')[3]
-    
-    if (elem.getAttribute('id') === url){
-        elem.classList.add('active')
-    } else {
-        if (elem.classList.contains('active')){
-            elem.classList.remove('active')
-        }
+links.forEach((elem) => {
+  const url = location.href.toString().split("/")[4];
+
+  if (elem.getAttribute("id") === "/bookclub/" + url) {
+    elem.classList.add("active");
+  } else {
+    if (elem.classList.contains("active")) {
+      elem.classList.remove("active");
     }
-    
-})
+  }
+});
